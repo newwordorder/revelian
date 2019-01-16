0709244ea865543ba6de9327d800923a7aece867
Nicholas MacArthur <n.j.macarthur@gmail.com>
2019-01-16 12:17:02 +1000

    Updates to the textsbox

--- page-templates/blocks/skillstests.php
+++ page-templates/blocks/skillstests.php
@@ -10,16 +10,21 @@ if( get_row_layout() == 'skills_tests' ):
     <div class="col-md-12">
     <div id="controls" class="controls py-2">
     <div class="row py-2"> 
-       <div class="col-md-5">
-          <select id="category" name="category" class="select mr-4">
-            <option value="all">All categories</option>
-          </select>
+       <div class="col-md-4">
+          <div class="select__wrapper" id="selectwrapper">
+            <select id="category" name="category" class="select mr-4">
+              <option value="all">All categories </option>
+            </select>
+          </div>
         </div>
-        <div class="col-md-5">
+        <div class="col-md-4">
           <input id="search" placeholder="Search with keyword"></input>
         </div>
-        <div class="col-md-2">
+        <div class="col-md-3">
         <button class="d-block btn btn--solid w-100 m-0" id="searchbtn">Search</button>
+        </div>
+        <div class="col-md-1">
+        <button class="d-block btn btn--outline w-100 m-0" id="clearbtn"><i class="fas fa-redo-alt"></i></button>
         </div>
     </div></div>
     </div>
